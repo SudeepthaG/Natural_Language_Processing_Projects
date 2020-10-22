e2ddee625e1ade59c97ca8b20a4eb4310305adcb
Sudeeptha Mouni Ganji <sganji@usc.edu>
2020-10-22 00:22:46 -0700

    Updating POS

--- POS_Tagger/hmmdecode.py
+++ POS_Tagger/hmmdecode.py
@@ -1,1 +1,105 @@
+import json
+import sys
 
+# f = open(sys.argv[1], encoding='UTF-8')
+# f = open('hmm-training-data/it_isdt_dev_raw.txt', encoding='UTF-8')
+f = open('hmm-training-data/ja_gsd_dev_raw.txt', encoding='UTF-8')
+model = open('hmmmodel.txt', encoding='UTF-8')
+inputdata = json.loads(model.read())
+model.close()
+outfile = open("hmmoutput.txt", mode='w', encoding='UTF-8')
+
+# for each sentence do this
+for line in f.readlines():
+    # get the words..
+    words = line.split()
+    tags_dict = inputdata[1]
+    words_tags_dict = inputdata[2]
+    res = {}  # 3d - matrix to store the data..
+    wordscount = len(words)+1
+    tagged_sent = ""
+    for i in range(0, wordscount):
+        res[i] = {}
+        if i == 0:
+            word = words[i]
+            if word in words_tags_dict.keys():
+                for tag in words_tags_dict[word].keys():
+                    res[i][tag] = {'prob':words_tags_dict[word][tag] + tags_dict['start'][tag], 'backptr':'start'}
+                continue
+            else:
+                for tag in inputdata[0].keys():
+                    if tag != 'start' and tag != 'end':
+                        res[i][tag] = {'prob':tags_dict['start'][tag], 'backptr':'start' }
+                continue
+
+        elif i == len(words):
+            maxValue = -sys.maxsize - 1
+            result = ''
+            for prev_tag,val in res[i - 1].items():
+                prob = res[i - 1][prev_tag]['prob'] + tags_dict[prev_tag]['end']
+                if maxValue < prob:
+                    maxValue = prob
+                    result = prev_tag
+                else:
+                    # print(prob+" checkpoint y"+maxValue)
+                    pass
+            res[i]['end'] = {'prob':maxValue, 'backptr' :result}
+            continue
+        else:
+            # print("not found")
+            pass
+        word = words[i]
+        if word in words_tags_dict.keys():
+            for tag,valu in words_tags_dict[word].items():
+                maxValue = -(sys.maxsize + 1)
+                result = ''
+                res[i][tag] = {}
+
+                for prev_tag,val in res[i - 1].items():
+                    prevProb=res[i - 1][prev_tag]['prob']
+                    prob = prevProb + words_tags_dict[word][tag] + tags_dict[prev_tag][
+                        tag]
+                    if  maxValue < prob:
+                        maxValue = prob
+                        result = prev_tag
+                    else:
+                        # print(prob+" checkpoint y"+maxValue)
+                        pass
+                res[i][tag] = {'prob':maxValue, 'backptr' :result}
+        else:
+            for tag in inputdata[0].keys():
+                if tag not in ['start','end']:
+                    maxValue = -(sys.maxsize + 1)
+                    res[i][tag] = {}
+                    result = ''
+                    for prev_tag,val in res[i - 1].items():
+                        prevProb=res[i - 1][prev_tag]['prob']
+                        prob = prevProb + tags_dict[prev_tag][tag]
+                        if maxValue < prob:
+                            maxValue = prob
+                            result = prev_tag
+                        else:
+                            # print(prob+" checkpoint z"+maxValue)
+                            pass
+                    res[i][tag] = {'prob':maxValue, 'backptr' :result}
+
+    # tag the results
+    length=len(res)
+    p = length - 1
+    q = length - 2
+    startTag = 'end'
+    while p - 1 >= 0:
+        tag = res[p][startTag]['backptr']
+        r = length - 1
+        if (p == r):
+            tagged_sent = words[q] + "/" + tag
+        elif (p !=r):
+            tagged_sent = words[q] + "/" + tag + " " + tagged_sent
+        startTag = tag
+        p -= 1
+        q -= 1
+    outfile.write(tagged_sent+ '\n')
+
+# write to the file
+outfile.close()
+f.close()
--- POS_Tagger/hmmlearn.py
+++ POS_Tagger/hmmlearn.py
@@ -1,1 +1,111 @@
+import json, sys, math
+
+tags_dict = {}
+no_of_tags = {}
+words_tags_dict = {}
+res = []
+
+# f = open('hmm-training-data/it_isdt_train_tagged.txt', encoding='UTF-8')
+f = open('hmm-training-data/ja_gsd_train_tagged.txt', encoding='UTF-8')
+# f = open(sys.argv[1], encoding='UTF-8')
+# print(sys.argv[1])
+lines = f.readlines()
+
+no_of_tags["start"]=len(lines)
+no_of_tags["end"]=len(lines)
+tagslist = ["start", "end"]
+# lines = f.readlines()
+# print(lines)
+
+for line in lines:
+    last_tag = ""
+    prev_tag = "start"
+    # print(line)
+    for word in line.split():
+
+        tag = word.split("/") [1]
+        tagslist.append(tag)
+        # increment the tag
+        if tag in no_of_tags.keys():
+            no_of_tags[tag] = no_of_tags[tag] + 1
+        else:
+            no_of_tags[tag]=1
+        currWord = word.split("/") [0]
+        # increment  word -> tag -> count
+        if currWord in words_tags_dict.keys():
+            no_of_tagsForGivenWord = words_tags_dict[currWord]
+        else:
+            words_tags_dict[currWord]={}
+            no_of_tagsForGivenWord = words_tags_dict[currWord]
+
+        if tag in no_of_tagsForGivenWord.keys():
+            no_of_tagsForGivenWord[tag] = no_of_tagsForGivenWord[tag] + 1
+        else:
+            no_of_tagsForGivenWord[tag]=1
+        words_tags_dict[currWord] = no_of_tagsForGivenWord 
+
+        # increment tag->tag -> count
+        if prev_tag in tags_dict.keys():
+            curr_tag_count = tags_dict[prev_tag]
+        else:
+            tags_dict[prev_tag]= {}
+            curr_tag_count = tags_dict[prev_tag]
+        if tag in curr_tag_count.keys():
+            curr_tag_count[tag] = curr_tag_count[tag] + 1
+        else:
+            curr_tag_count[tag] = 1
+        tags_dict[prev_tag] = curr_tag_count
+        last_tag = tag
+        prev_tag = tag 
+    # increment tag->tag -> count
+    if last_tag in tags_dict.keys():
+        curr_tag_count = tags_dict[last_tag]
+    else:
+        curr_tag_count = {}
+    if 'end' in curr_tag_count.keys():
+        curr_tag_count['end'] = curr_tag_count['end']+ 1
+    else:
+        curr_tag_count['end']=1
+    tags_dict[last_tag] = curr_tag_count
+
+for prev_tag in tags_dict.keys():
+    nextTag= tags_dict[prev_tag]
+    for tag in tagslist:
+        if tag in nextTag:
+            pass
+        else:
+            # print(tags_dict[prev_tag][tag])
+            tags_dict[prev_tag][tag] = 0
+
+
+
+res.append(no_of_tags)
+
+for prev_tag in tags_dict:
+    for curr_tag in tags_dict[prev_tag]:
+        a=tags_dict[prev_tag][curr_tag]
+        a+=1
+        a= math.log(a)
+        b=no_of_tags[prev_tag]
+        b+=1
+        b=math.log(b)
+        tags_dict[prev_tag][curr_tag] =a-b
+
+
+res.append(tags_dict)
+
+for word in words_tags_dict:
+    for tag in words_tags_dict[word]:
+        a=words_tags_dict[word][tag]
+        a=math.log(a)
+        b=no_of_tags[tag]
+        b=math.log(b)
+        words_tags_dict[word][tag] = a-b
+
+res.append(words_tags_dict)
+# print(res)
+modelfile = open('hmmmodel.txt', mode='w', encoding='UTF-8')
+modelfile.write(json.dumps(res))
+modelfile.close()
+
 
